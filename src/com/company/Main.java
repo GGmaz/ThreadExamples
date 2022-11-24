8112a676fe7ac9611e21bec7dc37faf86064b3d7
GGmaz <kajtes.nikola@gmail.com>
2022-11-24 13:22:55 +0100

    find min in array with 4 threads

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static class MyThread implements Runnable {
+        List<Integer> list;
+        int no;
+
+        public MyThread(List<Integer> list, int i) {
+            this.list = list;
+            this.no = i;
+        }
+
+        @Override
+        public void run() {
+            Integer min = 999;
+            for(Integer el : list)
+                if(el < min)
+                    min = el;
+            System.out.println("Thread" + no + " - " + min);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(11, 2, 234, 23, 12, 3, 2, 34, 7, 4, 678, 9, 78, 97, 5, 54, 69);
+
+        int i = 0;
+        int quarterSize = list.size() / 4;
+        while(quarterSize * i < list.size()) {
+            Thread thread;
+            if(quarterSize * i + 4 > list.size())
+                thread = new Thread(new MyThread(list.subList(quarterSize * i, list.size()), i));
+            else
+                thread = new Thread(new MyThread(list.subList(quarterSize * i, quarterSize * i + 4), i));
+            thread.start();
+            i++;
+        }
+
+        System.out.println("Hello");
+    }
+}
